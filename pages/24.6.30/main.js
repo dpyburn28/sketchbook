0a5a6dfa9551ea5a7b672d7b5e158f3a7754117b
birdmahn <123044295+dpyburn28@users.noreply.github.com>
2024-07-02 19:18:24 -0500

    spiral crafting

--- pages/24.6.30/main.js
+++ pages/24.6.30/main.js
@@ -1,7 +1,7 @@
 const backgroundCanvas = new myCanvas(
     res=[window.innerWidth, window.innerHeight],
     pos=[0,0],
-    color=[0,0,0,1]
+    color=[255,255,255,1]
 )
 
 const mainCanvas = new myCanvas(
@@ -11,10 +11,10 @@ const mainCanvas = new myCanvas(
 )
 
 // Initialize and Start timer
-let timer = new Timer(MS * 100)
+let timer = new Timer(MS * 50)
 
 
-mainCanvas.brush.ctx.lineWidth = 5
+mainCanvas.brush.ctx.lineWidth = .05
 mainCanvas.brush.pather.connectAnchor = false
 
 let increasePoints = (range=[0, 100]) => {
@@ -24,7 +24,6 @@ let increasePoints = (range=[0, 100]) => {
     let numPoints = min + (timer.value % max)
     let timerSin = Math.sin(timer.value / timer.rate)
     numPoints = Math.floor(timerSin * (range/2)) + min + (range/2)
-    mainCanvas.clear()
     mainCanvas.brush.draw("spiral", [r=200, swirl=20, numPoints=numPoints])
     mainCanvas.brush.ctx.stroke()
 }
@@ -32,6 +31,7 @@ let increasePoints = (range=[0, 100]) => {
 // Interval
 let interval = () => {
     timer.interval()
-    increasePoints(range=[20, 100])
+    mainCanvas.clear(.1)
+    increasePoints(range=[0, 200])
 }
 setInterval(interval, timer.rate)
