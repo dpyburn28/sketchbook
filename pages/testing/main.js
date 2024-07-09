f3c77cf1b7b6aa52d078180e7e0ad502dd630a59
birdmahn <123044295+dpyburn28@users.noreply.github.com>
2024-07-08 22:26:46 -0500

    bristles

--- pages/testing/main.js
+++ pages/testing/main.js
@@ -1,5 +1,5 @@
 
-let timer = new Timer(MS*100)
+let timer = new Timer(SECONDS/16)
 
 let canvas = new myCanvas(
     res=[window.innerWidth, window.innerHeight],
@@ -9,21 +9,33 @@ let canvas = new myCanvas(
 
 let brush = canvas.brush = new Brush(canvas)
 
-let p1 = [.25, .75]
-let p2 = [.75, .75]
-let c1 = [.75, .25]
-let c2 = [.25, .25]
+let p1 = [.75, .75]
+let p2 = [.75, .25]
+let p3 = [.25, .25]
+let p4 = [.25, .75]
 
-brush.presets("pencil")
-brush.isPaint = false
-// brush.weight = 30
-brush.closer = true
-
+brush.weight = 1
 brush.closer = false
-// brush.curve([p1, c1, c2, p2])
-brush.presets("brush")
+
+let draw = () => {
+    brush.presets("brush")
+    brush.weight = 1
+    
+    brush.bristles.num=5
+    brush.bristles.spread=10
+    brush.isPaint = false
+    brush.isInk = true
+    brush.closer = false
+    brush.curve([p1, p2, p3, p4])
+    brush.presets("pencil")
+    brush.tip = "bristles"
+    brush.line([p1, p2, p3, p4])
+
+    
+}
 let interval = () => {
     canvas.clear()
-    brush.line([p1, c1, c2, p2])
+    draw()
 }
+interval()
 timer.startInterval(interval)
